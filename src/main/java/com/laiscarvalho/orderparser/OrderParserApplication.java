1d5b5f1e32495c7a904f844a43586805263fdc75
Lais Soares de Carvalho <lais.carvalho@luizalabs.com>
2024-06-05 20:16:43 -0300

    feat: setup spring project

---
+++ src/main/java/com/laiscarvalho/orderparser/OrderParserApplication.java
@@ -0,0 +1,13 @@
+package com.laiscarvalho.orderparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderParserApplication.class, args);
+	}
+
+}
